8b0a60f13ee41cf8df5fd8dde143ecbe74e08b96
yearn <yearn_ym@163.com>
2020-02-28 00:30:13 +0800

    init project

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+interface Config {
+    width: number,
+    height: number,
+    path: string,
+    autoDispose?: boolean,
+    renderGif?: boolean,
+    canvas: any,
+    options: object // echarts options
+}
+
+import Gif from "gif.js";
+
+export default class EchartsSSR {
+    constructor(config: Config) {
+
+    }
+
+    render() {
+
+    }
+
+    export() {
+
+    }
+}
